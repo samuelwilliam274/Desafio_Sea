e9c4f2b5c910b3f8e992521a8828183814532ae7
samuelwilliam274 <samuelwilliam274@gmail.com>
2025-11-17 12:24:15 -0300

    Delete desafio-backend directory

--- desafio-backend/src/main/java/com/desafio/backend/repository/ClienteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.desafio.backend.repository;
-
-import com.desafio.backend.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
