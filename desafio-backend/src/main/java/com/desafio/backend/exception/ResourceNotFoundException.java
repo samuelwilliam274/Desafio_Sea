b533c42aa1f58c322b9522ec0b8bc64f97a6381e
samuelwilliam274 <samuelwilliam274@gmail.com>
2025-11-18 11:35:40 -0300

    Delete desafio-backend directory

--- desafio-backend/src/main/java/com/desafio/backend/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.desafio.backend.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
